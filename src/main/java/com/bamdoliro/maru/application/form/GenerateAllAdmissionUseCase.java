1e66aec40c8b0982318d2b8756c567b52c594dd7
jyj1289 <jin37091289@gmail.com>
2024-10-21 16:48:06 +0900

    feat(#163): 전체 수험표 다운로드 - 전체 수험표를 다운로드하는 API를 만들었어요.

---
+++ src/main/java/com/bamdoliro/maru/application/form/GenerateAllAdmissionUseCase.java
@@ -0,0 +1,70 @@
+package com.bamdoliro.maru.application.form;
+
+import com.bamdoliro.maru.domain.form.domain.Form;
+import com.bamdoliro.maru.domain.form.domain.type.FormStatus;
+import com.bamdoliro.maru.infrastructure.pdf.GeneratePdfService;
+import com.bamdoliro.maru.infrastructure.pdf.MergePdfService;
+import com.bamdoliro.maru.infrastructure.persistence.form.FormRepository;
+import com.bamdoliro.maru.infrastructure.s3.FileService;
+import com.bamdoliro.maru.infrastructure.s3.constants.FolderConstant;
+import com.bamdoliro.maru.infrastructure.thymeleaf.ProcessTemplateService;
+import com.bamdoliro.maru.infrastructure.thymeleaf.Templates;
+import com.bamdoliro.maru.shared.annotation.UseCase;
+import com.bamdoliro.maru.shared.constants.Schedule;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.utils.PdfMerger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+
+import java.util.List;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
+
+import static com.bamdoliro.maru.shared.constants.Schedule.*;
+
+@RequiredArgsConstructor
+@UseCase
+public class GenerateAllAdmissionUseCase {
+
+    private final FormRepository formRepository;
+    private final ProcessTemplateService processTemplateService;
+    private final GeneratePdfService generatePdfService;
+    private final MergePdfService mergePdfService;
+    private final FileService fileService;
+
+    public ByteArrayResource execute() {
+        List<Form> formList = formRepository.findByStatus(FormStatus.FIRST_PASSED);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PdfDocument mergedDocument = new PdfDocument(new PdfWriter(outputStream));
+        PdfMerger pdfMerger = new PdfMerger(mergedDocument);
+
+        formList.forEach(form -> mergePdfService.execute(pdfMerger, generateAdmissionTicket(form)));
+
+        mergedDocument.close();
+        pdfMerger.close();
+
+        return new ByteArrayResource(outputStream.toByteArray());
+    }
+
+    private ByteArrayOutputStream generateAdmissionTicket(Form form) {
+
+        Map<String, Object> formMap = Map.ofEntries(
+                Map.entry("form", form),
+                Map.entry("year", Schedule.getAdmissionYear()),
+                Map.entry("codingTest", Schedule.toLocaleString(CODING_TEST)),
+                Map.entry("ncs", Schedule.toLocaleString(NCS)),
+                Map.entry("depthInterview", Schedule.toLocaleString(DEPTH_INTERVIEW)),
+                Map.entry("physicalExamination", Schedule.toLocaleString(PHYSICAL_EXAMINATION)),
+                Map.entry("announcementOfSecondPass", Schedule.toLocaleString(ANNOUNCEMENT_OF_SECOND_PASS)),
+                Map.entry("meisterTalentEntranceTime", Schedule.toLocaleString(MEISTER_TALENT_ENTRANCE_TIME)),
+                Map.entry("meisterTalentExclusionEntranceTime", Schedule.toLocaleString(MEISTER_TALENT_EXCLUSION_ENTRANCE_TIME)),
+                Map.entry("entranceRegistrationTime", Schedule.toLocaleString(ENTRANCE_REGISTRATION_PERIOD_START, ENTRANCE_REGISTRATION_PERIOD_END)),
+                Map.entry("identificationPictureUri", fileService.getPresignedUrl(FolderConstant.IDENTIFICATION_PICTURE, form.getUser().getUuid().toString()).getDownloadUrl())
+        );
+        String html = processTemplateService.execute(Templates.ADMISSION_TICKET, formMap);
+
+        return generatePdfService.execute(html);
+    }
+}
