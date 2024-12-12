af2ba5f5bd0262dde0376ba79d3f99f8da9dc57a
jyj1289 <jin37091289@gmail.com>
2024-12-12 19:00:22 +0900

    fix(#167): WebConfig origin 제거 - localhost:3000 등의 origin을 추가해놓으면 서버에 접근할 수 있기때문에 제거했어요.

--- src/main/java/com/bamdoliro/maru/shared/config/WebConfig.java
+++ src/main/java/com/bamdoliro/maru/shared/config/WebConfig.java
@@ -25,9 +25,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:3000",
-                        "http://localhost:3001",
-                        "http://localhost:3002",
                         "https://maru.bamdoliro.com",
                         "https://madmin.bamdoliro.com"
                 )
