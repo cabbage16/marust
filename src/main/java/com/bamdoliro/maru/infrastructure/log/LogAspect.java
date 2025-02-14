64918b2811c4b9654492859d749588542202c742
jyj1289 <jin37091289@gmail.com>
2025-02-14 19:41:35 +0900

    feat(#175): LogAspect 작성 - 로깅을 독립적인 코드로 분리하기 위해서 AOP를 이용하여 LogAspect를 구현했어요.

---
+++ src/main/java/com/bamdoliro/maru/infrastructure/log/LogAspect.java
@@ -0,0 +1,44 @@
+package com.bamdoliro.maru.infrastructure.log;
+
+import com.bamdoliro.maru.domain.log.AdminLoginLog;
+import com.bamdoliro.maru.domain.user.domain.User;
+import com.bamdoliro.maru.domain.user.domain.type.Authority;
+import com.bamdoliro.maru.domain.user.service.UserFacade;
+import com.bamdoliro.maru.infrastructure.persistence.log.AdminLoginLogRepository;
+import com.bamdoliro.maru.presentation.auth.dto.request.LogInRequest;
+import com.bamdoliro.maru.presentation.auth.dto.response.TokenResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@RequiredArgsConstructor
+@Aspect
+@Component
+public class LogAspect {
+
+    private final AdminLoginLogRepository adminLoginLogRepository;
+    private final UserFacade userFacade;
+
+    @AfterReturning(value = "execution(* com.bamdoliro.maru.application.auth.LogInUseCase.execute(..))")
+    public void logAdminLoginSuccess(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        String phoneNumber = ((LogInRequest) args[0]).getPhoneNumber();
+
+        User user = userFacade.getUser(phoneNumber);
+
+        if (user.getAuthority() == Authority.ADMIN) {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+            String clientIp = request.getRemoteAddr();
+            String userAgent = request.getHeader("User-Agent");
+
+            AdminLoginLog log = new AdminLoginLog(phoneNumber, clientIp, userAgent, user);
+            adminLoginLogRepository.save(log);
+
+        }
+    }
+}
