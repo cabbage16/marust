0fc19462ebc96193e608a8235176d6d10a6cbc60
cabbage16 <qocn16@gmail.com>
2025-02-20 21:22:50 +0900

    refactor(#173): 기존 S3 파일 업로드 방식 코드 삭제 - 사용되지 않는 S3 파일 업로드 코드를 삭제했어요.

--- src/main/java/com/bamdoliro/maru/infrastructure/s3/FileService.java
+++ src/main/java/com/bamdoliro/maru/infrastructure/s3/FileService.java
@@ -1,58 +1,27 @@
 package com.bamdoliro.maru.infrastructure.s3;
 
 import com.amazonaws.HttpMethod;
-import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.Headers;
 import com.amazonaws.services.s3.model.*;
 import com.bamdoliro.maru.infrastructure.s3.dto.response.UrlResponse;
-import com.bamdoliro.maru.infrastructure.s3.exception.FailedToSaveException;
 import com.bamdoliro.maru.infrastructure.s3.validator.FileValidator;
 import com.bamdoliro.maru.infrastructure.s3.dto.request.FileMetadata;
-import com.bamdoliro.maru.shared.config.properties.S3Properties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
 
-    private final S3Properties s3Properties;
     private final AmazonS3Client amazonS3Client;
 
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
-
-
-    /** @deprecated */
-    public UrlResponse execute(MultipartFile file, String folder, String fileName, FileValidator validator) {
-//        validator.validate(file);
-        String fullFileName = createFileName(folder, fileName);
-
-        try {
-            PutObjectRequest request = new PutObjectRequest(
-                    s3Properties.getBucket(),
-                    fullFileName,
-                    file.getInputStream(),
-                    getObjectMetadata(file)
-            ).withCannedAcl(CannedAccessControlList.PublicRead);
-
-            amazonS3Client.putObject(request);
-        } catch (SdkClientException | IOException e) {
-            throw new FailedToSaveException();
-        }
-
-        return new UrlResponse(
-                amazonS3Client.getUrl(s3Properties.getBucket(), fullFileName).toString(),
-                amazonS3Client.getUrl(s3Properties.getBucket(), fullFileName).toString()
-        );
-    }
 
     public String getUploadPresignedUrl(String folder, String fileName, FileMetadata fileMetadata, FileValidator validator) {
         validator.validate(fileMetadata);
@@ -113,12 +82,5 @@ public class FileService {
 
     private String createFileName(String folder, String fileName) {
         return folder + "/" + fileName;
-    }
-
-    private ObjectMetadata getObjectMetadata(MultipartFile file) {
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentLength(file.getSize());
-        objectMetadata.setContentType(file.getContentType());
-        return objectMetadata;
     }
 }
--- src/test/java/com/bamdoliro/maru/infrastructure/s3/FileServiceTest.java
+++ src/test/java/com/bamdoliro/maru/infrastructure/s3/FileServiceTest.java
@@ -4,11 +4,9 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.bamdoliro.maru.infrastructure.s3.dto.request.FileMetadata;
 import com.bamdoliro.maru.infrastructure.s3.dto.response.UrlResponse;
-import com.bamdoliro.maru.infrastructure.s3.exception.EmptyFileException;
-import com.bamdoliro.maru.infrastructure.s3.exception.FailedToSaveException;
-import com.bamdoliro.maru.infrastructure.s3.exception.FileSizeLimitExceededException;
-import com.bamdoliro.maru.infrastructure.s3.exception.ImageSizeMismatchException;
+import com.bamdoliro.maru.infrastructure.s3.exception.*;
 import com.bamdoliro.maru.shared.config.properties.S3Properties;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -43,183 +41,31 @@ class FileServiceTest {
     private FileService fileService;
 
     @Mock
-    private S3Properties s3Properties;
-
-    @Mock
     private AmazonS3Client amazonS3Client;
 
     @Test
-    void 파일을_업로드한다() throws MalformedURLException {
+    void Presigned_URL을_생성한다() throws Exception {
         // given
-        MockMultipartFile image = new MockMultipartFile("image", "image.png", MediaType.IMAGE_PNG_VALUE, "<<image>>".getBytes(StandardCharsets.UTF_8));
         String url = "https://bucket.s3.ap-northeast-2.amazonaws.com/random-uuid-image.png";
-        given(s3Properties.getBucket()).willReturn("bucket");
-        given(amazonS3Client.putObject(any(PutObjectRequest.class))).willReturn(null);
-        given(amazonS3Client.getUrl(any(String.class), any(String.class))).willReturn(new URL(url));
-
-        // when
-        UrlResponse response = fileService.execute(image, "folder", "form-uuid", file -> {});
-
-        // then
-        assertEquals(url, response.getDownloadUrl());
-        assertEquals(url, response.getUploadUrl());
-        verify(s3Properties, times(3)).getBucket();
-        verify(amazonS3Client, times(1)).putObject(any(PutObjectRequest.class));
-        verify(amazonS3Client, times(2)).getUrl(any(String.class), any(String.class));
-    }
-
-    @Test
-    void 파일을_업로드할_떄_파일이_비어있으면_에러가_발생한다() {
-        // given
-        MockMultipartFile image = new MockMultipartFile("image", "image.png", MediaType.IMAGE_PNG_VALUE, "".getBytes(StandardCharsets.UTF_8));
-
-        // when and then
-        assertThrows(EmptyFileException.class, () -> fileService.execute(image, "folder", "form-uuid", file -> {
-        }));
-
-        verify(s3Properties, never()).getBucket();
-        verify(amazonS3Client, never()).putObject(any(PutObjectRequest.class));
-        verify(amazonS3Client, never()).getUrl(any(String.class), any(String.class));
-    }
-
-    @Test
-    void 파일을_업로드할_때_업로드가_실패하면_에러가_발생한다() {
-        // given
-        MockMultipartFile image = new MockMultipartFile("image", "image.png", MediaType.IMAGE_PNG_VALUE, "<<image>>".getBytes(StandardCharsets.UTF_8));
-        given(s3Properties.getBucket()).willReturn("bucket");
-        willThrow(AmazonServiceException.class).given(amazonS3Client).putObject(any(PutObjectRequest.class));
-
-        // when and then
-        assertThrows(FailedToSaveException.class, () -> fileService.execute(image, "folder", "form-uuid", file -> {
-        }));
-
-        verify(s3Properties, times(1)).getBucket();
-        verify(amazonS3Client, times(1)).putObject(any(PutObjectRequest.class));
-        verify(amazonS3Client, never()).getUrl(any(String.class), any(String.class));
-    }
-
-    @Test
-    void 파일을_업로드할_때_파일_용량이_크면_에러가_발생한다() {
-        // given
-        MockMultipartFile image = new MockMultipartFile(
-                "image",
+        FileMetadata fileMetadata = new FileMetadata(
                 "image.png",
-                MediaType.IMAGE_PNG_VALUE,
-                new byte[2 * 1024 * 1024 + 1]
+                MediaType.IMAGE_PNG,
+                25099L
         );
-
-        // when and then
-        assertThrows(
-                FileSizeLimitExceededException.class,
-                () -> fileService.execute(image, "folder", "form-uuid", file -> {
-                    if (file.getSize() > 2 * MB) {
-                        throw new FileSizeLimitExceededException();
-                    }
-                })
-        );
-
-        // then
-        verify(s3Properties, never()).getBucket();
-        verify(amazonS3Client, never()).putObject(any(PutObjectRequest.class));
-        verify(amazonS3Client, never()).getUrl(any(String.class), any(String.class));
-    }
-
-    @Test
-    void 파일을_업로드할_때_검증_함수를_통해_이미지_크기를_확인한다() throws IOException {
-        // given
-        String fileName = "id-picture.png";
-        MockMultipartFile image = new MockMultipartFile(
-                "image",
-                fileName,
-                MediaType.IMAGE_PNG_VALUE,
-                getFileStreamWith(fileName)
-        );
-
-        String url = "https://bucket.s3.ap-northeast-2.amazonaws.com/random-uuid-image.png";
-        given(s3Properties.getBucket()).willReturn("bucket");
-        given(amazonS3Client.putObject(any(PutObjectRequest.class))).willReturn(null);
-        given(amazonS3Client.getUrl(any(String.class), any(String.class))).willReturn(new URL(url));
+        given(amazonS3Client.generatePresignedUrl(any(GeneratePresignedUrlRequest.class))).willReturn(new URL(url));
 
         // when
-        UrlResponse response = fileService.execute(image, "folder", "form-uuid", file -> {
-            try {
-                BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
-                if (!(bufferedImage.getWidth() == 117 && bufferedImage.getHeight() == 156)) {
-                    throw new ImageSizeMismatchException();
-                }
-            } catch (IOException e) {
-                throw new FailedToSaveException();
+        UrlResponse response = fileService.getPresignedUrl("folder", "uuid", fileMetadata, metadata -> {
+            if (!(metadata.getMediaType().equals(MediaType.IMAGE_PNG) || metadata.getMediaType().equals(MediaType.IMAGE_JPEG))) {
+                throw new MediaTypeMismatchException();
+            }
+
+            if (metadata.getFileSize() > 2 * MB) {
+                throw new FileSizeLimitExceededException();
             }
         });
 
         // then
-        assertEquals(url, response.getDownloadUrl());
-        assertEquals(url, response.getUploadUrl());
-        verify(s3Properties, times(3)).getBucket();
-        verify(amazonS3Client, times(1)).putObject(any(PutObjectRequest.class));
-        verify(amazonS3Client, times(2)).getUrl(any(String.class), any(String.class));
-    }
-
-    @Test
-    void 파일을_업로드할_때_이미지_크기가_크면_에러가_발생한다() throws IOException {
-        // given
-        String fileName = "id-picture-big.png";
-        MockMultipartFile image = new MockMultipartFile(
-                "image",
-                fileName,
-                MediaType.IMAGE_PNG_VALUE,
-                getFileStreamWith(fileName)
-        );
-
-        // when and then
-        assertThrows(
-                ImageSizeMismatchException.class,
-                () -> fileService.execute(image, "folder", "form-uuid", file -> {
-                    try {
-                        BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
-                        if (!(bufferedImage.getWidth() == 117 && bufferedImage.getHeight() == 156)) {
-                            throw new ImageSizeMismatchException();
-                        }
-                    } catch (IOException e) {
-                        throw new FailedToSaveException();
-                    }
-                })
-        );
-
-        // then
-        verify(s3Properties, never()).getBucket();
-        verify(amazonS3Client, never()).putObject(any(PutObjectRequest.class));
-        verify(amazonS3Client, never()).getUrl(any(String.class), any(String.class));
-    }
-
-    @Test
-    void presigned_URL을_생성한다() throws Exception {
-        // given
-        String url = "https://bucket.s3.ap-northeast-2.amazonaws.com/random-uuid-image.png";
-        given(amazonS3Client.generatePresignedUrl(any(GeneratePresignedUrlRequest.class))).willReturn(new URL(url));
-
-        // when
-        UrlResponse response = fileService.getPresignedUrl("folder", "uuid");
-
-        // then
         verify(amazonS3Client, times(2)).generatePresignedUrl(any(GeneratePresignedUrlRequest.class));
-    }
-
-    @Test
-    void presigned_URL이_만료되면_에러가_발생한다() throws Exception {
-        // given
-        String url = "https://bucket.s3.ap-northeast-2.amazonaws.com/random-uuid-image.png";
-        given(amazonS3Client.generatePresignedUrl(any(GeneratePresignedUrlRequest.class))).willReturn(new URL(url));
-
-        // when
-        UrlResponse response = fileService.getPresignedUrl("folder", "uuid");
-
-        // then
-        verify(amazonS3Client, times(2)).generatePresignedUrl(any(GeneratePresignedUrlRequest.class));
-    }
-
-    private FileInputStream getFileStreamWith(String fileName) throws FileNotFoundException {
-        final String path = String.format("src/test/resources/images/%s", fileName);
-        return new FileInputStream(path);
     }
 }
