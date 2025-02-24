c2a0f337e256cd347f9f6f5f2da863d490dfbd3a
cabbage16 <qocn16@gmail.com>
2025-02-24 19:55:54 +0900

    perf(#173): 에러메시지에 String format을 사용할 수 있도록 개선했어요.

--- src/main/java/com/bamdoliro/maru/shared/error/MaruException.java
+++ src/main/java/com/bamdoliro/maru/shared/error/MaruException.java
@@ -1,11 +1,21 @@
 package com.bamdoliro.maru.shared.error;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public abstract class MaruException extends RuntimeException {
 
     private final ErrorProperty errorProperty;
+
+    public MaruException(ErrorProperty errorProperty, Object... args) {
+        super(String.format(errorProperty.getMessage(), args));
+        this.errorProperty = errorProperty;
+    }
+
+    public MaruException(ErrorProperty errorProperty) {
+        super(errorProperty.getMessage());
+        this.errorProperty = errorProperty;
+    }
 }
