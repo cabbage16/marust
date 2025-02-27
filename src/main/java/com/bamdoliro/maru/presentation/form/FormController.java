bf6dfca1dd55f7b02c201e9c02458aeae656aa72
cabbage16 <qocn16@gmail.com>
2025-02-27 23:37:26 +0900

    refactor(#173): 사용되지 않는 import 문 삭제

--- src/main/java/com/bamdoliro/maru/presentation/form/FormController.java
+++ src/main/java/com/bamdoliro/maru/presentation/form/FormController.java
@@ -32,8 +32,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
