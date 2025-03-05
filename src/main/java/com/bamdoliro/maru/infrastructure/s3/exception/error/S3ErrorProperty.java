9658c9191abab7b30a68cbb0c737887607e1ba06
cabbage16 <qocn16@gmail.com>
2025-03-05 20:10:37 +0900

    perf(#173): 공지사항 파일 형식 제한 - XSS 공격 방지를 위해서 어드민 공지사항에 들어갈 수 있는 파일을 이미지, octet-stream(.hwp)파일을, pdf, docx, pptx, xlsx로 제한했어요.

--- src/main/java/com/bamdoliro/maru/infrastructure/s3/exception/error/S3ErrorProperty.java
+++ src/main/java/com/bamdoliro/maru/infrastructure/s3/exception/error/S3ErrorProperty.java
@@ -16,7 +16,7 @@ public enum S3ErrorProperty implements ErrorProperty {
     FILE_SIZE_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "최대 %dMB의 파일까지만 업로드할 수 있습니다."),
     MEDIA_TYPE_MISMATCH(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "파일 형식이 다릅니다."),
     INVALID_MEDIA_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "파일 형식이 유효하지 않습니다."),
-    FILE_COUNT_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "최대 %d개의 파일까지만 업로드할 수 있습니다.")
+    FILE_COUNT_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "1~%d개의 파일까지만 업로드할 수 있습니다.")
     ;
 
     private final HttpStatus status;
