e68f8821946a4f657e7d3e0b9712040376ce713a
cabbage16 <qocn16@gmail.com>
2025-03-19 01:59:54 +0900

    feat(#171): 회원 탈퇴 API 구현 - 원서를 작성한 유저의 경우 원서를 삭제하고, 이후 리프레시 토큰, 유저순으로 삭제하도록 회원 탈퇴 API를 구현했어요.

---
+++ src/main/java/com/bamdoliro/maru/application/user/DeleteUserUseCase.java
@@ -0,0 +1,37 @@
+package com.bamdoliro.maru.application.user;
+
+import com.bamdoliro.maru.application.auth.LogOutUseCase;
+import com.bamdoliro.maru.domain.form.domain.Form;
+import com.bamdoliro.maru.domain.user.domain.User;
+import com.bamdoliro.maru.domain.user.domain.value.Password;
+import com.bamdoliro.maru.domain.user.exception.PasswordMismatchException;
+import com.bamdoliro.maru.infrastructure.persistence.form.FormRepository;
+import com.bamdoliro.maru.infrastructure.persistence.user.UserRepository;
+import com.bamdoliro.maru.presentation.user.dto.request.DeleteUserRequest;
+import com.bamdoliro.maru.shared.annotation.UseCase;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@UseCase
+public class DeleteUserUseCase {
+
+    private final UserRepository userRepository;
+    private final FormRepository formRepository;
+    private final LogOutUseCase logOutUseCase;
+
+    public void execute(User user, DeleteUserRequest request) {
+        validatePassword(request.getPassword(), user.getPassword());
+        Optional<Form> form = formRepository.findByUser(user);
+        form.ifPresent(formRepository::delete);
+        logOutUseCase.execute(user);
+        userRepository.delete(user);
+    }
+
+    private void validatePassword(String actual, Password expected) {
+        if (!expected.match(actual)) {
+            throw new PasswordMismatchException();
+        }
+    }
+}
