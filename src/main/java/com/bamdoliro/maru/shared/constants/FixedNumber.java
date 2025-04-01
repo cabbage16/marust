5b988ecfe95b3b516ac603d4638b80dc3ae35924
jyj1289 <jin37091289@gmail.com>
2025-04-01 09:40:10 +0900

    fix(#200): FixedNumber 값 복구 - 테스트를 위해 변경했던 값을 원래 값으로 복구했어요.

--- src/main/java/com/bamdoliro/maru/shared/constants/FixedNumber.java
+++ src/main/java/com/bamdoliro/maru/shared/constants/FixedNumber.java
@@ -5,12 +5,12 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class FixedNumber {
 
-    public static final int TOTAL = 18;
-    public static final int REGULAR = 6;
-    public static final int MEISTER_TALENT = 6;
+    public static final int TOTAL = 64;
+    public static final int REGULAR = 36;
+    public static final int MEISTER_TALENT = 22;
     public static final int SOCIAL_INTEGRATION = 6;
-    public static final int NATIONAL_VETERANS_EDUCATION = 0;
-    public static final int SPECIAL_ADMISSION = 0;
+    public static final int NATIONAL_VETERANS_EDUCATION = 2;
+    public static final int SPECIAL_ADMISSION = 1;
     public static final double MULTIPLE = 1.3;
     public static final double OTHER_REGION_RATE = 0.5;
 }
