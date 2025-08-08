4bd0dc062458b9318074469c44cc362ab13f7022
cabbage16 <qocn16@gmail.com>
2025-08-08 15:18:21 +0900

    chore: delete /fail api
    - 불필요한 API 삭제

--- marust/src/main.rs
+++ marust/src/main.rs
@@ -1,21 +1,16 @@
 mod common;
 
-use axum::{Router, routing::get};
-use common::{ApiResponse, AppError};
+use axum::{routing::get, Router};
+use common::ApiResponse;
 
 async fn health_check() -> ApiResponse<&'static str> {
     ApiResponse::ok("ok")
 }
 
-async fn always_fail() -> Result<ApiResponse<&'static str>, AppError> {
-    Err(AppError::InternalServerError)
-}
-
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/health", get(health_check))
-        .route("/fail", get(always_fail));
+        .route("/health", get(health_check));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
